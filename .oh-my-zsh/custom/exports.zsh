7ffb55cd42421dcc16ef4bbf468a71cbc96da7d5
Bernard Potocki <bernard.potocki@imanel.org>
2014-12-31 15:21:45 +0100

    Export boot2docker env

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -2,4 +2,6 @@
 export EDITOR="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl -w"
 
 # Docker host for boot2docker
-export DOCKER_HOST=tcp://localhost:2375
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/imanel/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
