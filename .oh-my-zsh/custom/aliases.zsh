9951bb640d84b05ab5716447704efc6923263ce5
Bernard Potocki <bernard.potocki@imanel.org>
2015-01-08 17:37:12 +0100

    Add adlias for gcm and remove percent symbol at the ond of line (see http://unix.stackexchange.com/a/167600)

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,2 @@
+alias gcm='git commit -m'
+compdef _git gcm=git-commit
--- .oh-my-zsh/custom/prompt.zsh
+++ .oh-my-zsh/custom/prompt.zsh
@@ -1,6 +1,7 @@
 # PROMPT='%{$fg_no_bold[green]%}%3~$(git_prompt_info)%{$reset_color%}$ '
 PROMPT='%{$fg[cyan]%}%~ $(git_prompt_info)%{$reset_color%}$ '
 RPROMPT='%{$FG[232]%}[%*]%{$reset_color%}'
+PROMPT_EOL_MARK=''
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}("
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[green]%}) "
